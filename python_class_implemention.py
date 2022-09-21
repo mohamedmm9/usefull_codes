3d930b317ee61e9fffd6be5b8faf9d9110565027
mohamed meqlad <76888049+mohamedmm9@users.noreply.github.com>
2022-09-21 17:18:40 +0200

    Create python_class_implemention.py

---
+++ python_class_implemention.py
@@ -0,0 +1,15 @@
+class point():
+    def __init__(slef, x, y):
+        self.x= x
+        self.y= try:
+            def getX(self):
+                return slef.x
+point1=point()
+point2=point()
+point1.x = 5
+point2.x = 10
+
+print(point1.getX())
+
+
+print(point2.getX())
